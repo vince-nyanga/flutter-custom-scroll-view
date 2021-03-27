a636466567515574a2b3bb5975ca7f8fda9d4baf
Vincent Nyanga <vince.nyanga@hotmail.com>
2021-03-27 16:13:49 +0200

    minor update

--- src/lib/pages/home_page.dart
+++ src/lib/pages/home_page.dart
@@ -9,23 +9,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(child:  SafeArea(
-      child: CustomScrollView(
-      slivers: [
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('My People'),
-        ),
-        SliverList(delegate: SliverChildBuilderDelegate((context, index) {
-          final person = people[index];
-          return Material(
-            child: ListTile(
-              title: Text(person.name),
-              subtitle: Text('${person.age}'),
-            ),
-          );
-        }, 
-        childCount: people.length,
-        ))
+    return CupertinoPageScaffold(
+      child:  SafeArea(
+        top: false,
+        child: CustomScrollView(
+        slivers: [
+          CupertinoSliverNavigationBar(
+            largeTitle: Text('My People'),
+          ),
+          SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+            final person = people[index];
+            return Material(
+              child: ListTile(
+                title: Text(person.name),
+                subtitle: Text('${person.age}'),
+              ),
+            );
+          }, 
+          childCount: people.length,
+          ))
       ],
     )));
   }
